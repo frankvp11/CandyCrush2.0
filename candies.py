546a9022402eb3c088868befc077678a319e9c16
frankvp <90110502+frankvp11@users.noreply.github.com>
2023-03-04 14:52:28 -0500

    Add files via upload

--- candies.py
+++ candies.py
@@ -9,7 +9,7 @@ screen_height = 1280
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 clock = pygame.time.Clock()
-font = pygame.font.Font('freesansbold.ttf', 50)
+font = pygame.font.Font('freesansbold.ttf', 50) 
 white = (255, 255, 255)
 green = (0, 255, 0)
 blue = (0, 0, 128)
@@ -19,7 +19,12 @@ mapping = {1: '/home/frankvp11/CCWebsite/MyAttempt/candies/Blue.png',
            4:'/home/frankvp11/CCWebsite/MyAttempt/candies/Purple.png',
            5:'/home/frankvp11/CCWebsite/MyAttempt/candies/Red.png',
            6:'/home/frankvp11/CCWebsite/MyAttempt/candies/Yellow.png',
-           0: '/home/frankvp11/CCWebsite/MyAttempt/candies/BlueWrapped.png'}
+           7: '/home/frankvp11/CCWebsite/MyAttempt/candies/BlueWrapped.png',
+           8:'/home/frankvp11/CCWebsite/MyAttempt/candies/GreenWrapped.png',
+           9:'/home/frankvp11/CCWebsite/MyAttempt/candies/OrangeWrapped.png',
+           10:'/home/frankvp11/CCWebsite/MyAttempt/candies/PurpleWrapped.png',
+           11:'/home/frankvp11/CCWebsite/MyAttempt/candies/RedWrapped.png',
+           12:'/home/frankvp11/CCWebsite/MyAttempt/candies/YellowWrapped.png'}
 prev_pos = None
 
 running = True
@@ -105,7 +110,7 @@ class Candy:
                         self.rect.topleft = self.originalx, (event.pos[1] - self.rel_pos[1])
 
     def is_valid(self, i, j, original_value):
-        if (i >= 0 and i < len(self.grid) and j >= 0 and j < len(self.grid[i]) and self.grid[i][j] == original_value and not self.visited[i][j] and original_value != 0):
+        if (i >= 0 and i < len(self.grid) and j >= 0 and j < len(self.grid[i]) and (self.grid[i][j] == original_value or abs(self.grid[i][j]-original_value)==6) and not self.visited[i][j]):
             return True
         else:
             return False
@@ -144,13 +149,21 @@ class Candy:
         self.grid[x1][y1] = temp2
 
     def customBFS(self, i, j, original_value):
+        global score
         self.visited = [[False for _ in range(len(self.grid))] for _ in range(len(self.grid[0]))]
         queue = deque([(i, j)])
         size = 1
         while (len(queue) != 0):
             r, c=  queue.popleft()
             self.visited[r][c] = True
-            self.grid[r][c] = 0
+            if (self.grid[r][c] >= 7):
+                for x, y in [(1, 1), (1, 0), (1, -1), (0, 1), (0, 0), (0,-1), (-1, 1), (-1, 0), (-1, -1)]:
+                    try:
+                        grid[r+x][c+y] = 0
+                    except IndexError:
+                        continue
+                score += 5000
+            grid[r][c] = 0
             for r_move, c_move in ((-1,0),(1,0),(0,1),(0,-1)):
                 if (self.is_valid(r+r_move, c+c_move, original_value)):
                     queue.append((r+r_move, c+c_move))
@@ -167,8 +180,15 @@ class Candy:
         global score, score_text
         if (direction == 'right'):
             self.swap(i, j, i, j+1)
-            if (self.isAcceptableMove(i, j+1)):
-                size = self.customBFS(i, j+1, self.grid[i][j+1])
+            color = self.grid[i][j+1]
+            size = self.bfs(i, j+1, color)
+            if (size > 2):
+                size = self.customBFS(i, j+1, color)
+                if (size >= 4):
+                    if (color > 6):
+                        self.grid[i][j+1] = color
+                    else:
+                        self.grid[i][j+1] = color+6
                 score += (size*15)
                 score_text = font.render("Score: {}".format(score), True, green)
                 self.gravity()
@@ -177,8 +197,15 @@ class Candy:
 
         elif (direction == 'left'):
             self.swap(i, j, i, j-1)
-            if (self.isAcceptableMove(i, j-1)):
-                size = self.customBFS(i, j-1, self.grid[i][j-1])
+            color = self.grid[i][j-1]
+            size = self.bfs(i, j-1, color)
+            if (size > 2):
+                size = self.customBFS(i, j-1, color)
+                if (size >= 4):
+                    if (color > 6):
+                        self.grid[i][j-1] = color
+                    else:
+                        self.grid[i][j-1] = color+6
                 score += (size* 15)
                 score_text = font.render("Score: {}".format(score), True, green)
                 self.gravity()
@@ -187,8 +214,15 @@ class Candy:
 
         elif (direction == 'down'):
             self.swap(i, j, i+1, j)
-            if (self.isAcceptableMove(i+1, j)):
+            color = self.grid[i+1][j]
+            size = self.bfs(i+1, j, color)
+            if (size > 2):
                 size = self.customBFS(i+1, j, self.grid[i+1][j])
+                if (size >=4 ):
+                    if (color > 6):
+                        self.grid[i+1][j] = color
+                    else:
+                        self.grid[i+1][j] = color+6
                 score += (size * 15)
                 score_text = font.render("Score: {}".format(score), True, green)
                 self.gravity()
@@ -196,8 +230,15 @@ class Candy:
                 self.swap(i, j, i+1, j)
         elif (direction == 'up'):
             self.swap(i, j, i-1, j)
-            if (self.isAcceptableMove(i-1, j)):
-                size = self.customBFS(i-1, j, self.grid[i-1][j])
+            color = self.grid[i-1][j]
+            size = self.bfs(i-1, j, color) 
+            if (size > 2):
+                size =self.customBFS(i-1, j, self.grid[i-1][j])
+                if (size >= 4):
+                    if (color > 6):
+                        self.grid[i-1][j] = color
+                    else:
+                        self.grid[i-1][j] = color+6
                 score += (size * 15)
                 
                 self.gravity()
@@ -239,10 +280,10 @@ class Candy:
              return False
 
 def create_grid():
-    return [[random.randint(1, 6) for _ in range(5)] for i in range(5)]
+    return [[random.randint(1, 6) for _ in range(8)] for i in range(8)]
 grid = create_grid()
 
-new_grid = [[None for _ in range(5)] for i in range(5)]
+new_grid = [[None for _ in range(8)] for i in range(8)]
 def renew_grid(grid):
     any = False
     for i in range(len(grid)):
@@ -253,78 +294,26 @@ def renew_grid(grid):
     if (any):
         return
     else:
-        print("Creating new grid!")
         grid2 = create_grid()
         renew_grid(grid2)
 
 renew_grid(grid)
 
-class Button():
-	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
-		self.image = image
-		self.x_pos = pos[0]
-		self.y_pos = pos[1]
-		self.font = font
-		self.base_color, self.hovering_color = base_color, hovering_color
-		self.text_input = text_input
-		self.text = self.font.render(self.text_input, True, self.base_color)
-		if self.image is None:
-			self.image = self.text
-		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
-		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
-
-	def update(self, screen):
-		if self.image is not None:
-			screen.blit(self.image, self.rect)
-		screen.blit(self.text, self.text_rect)
-
-	def checkForInput(self, position):
-		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-			return True
-		return False
-
-	def changeColor(self, position):
-		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-			self.text = self.font.render(self.text_input, True, self.hovering_color)
-		else:
-			self.text = self.font.render(self.text_input, True, self.base_color)
 
 
 find_move = False
 while running:
         screen.fill("black")
-        help_button = Button(image=None, pos=(800, 200), text_input='help', font=font, base_color='white', hovering_color='green')
 
         screen.blit(score_text, score_rect)
-        screen.blit(help_text, help_rect)
         mouse_pos = pygame.mouse.get_pos()
-        help_button.changeColor(mouse_pos)
-        help_button.update(screen)
-
-
-
         events_list = pygame.event.get()
         for event in events_list:
             if (event.type == pygame.QUIT):
-                running = False 
-            if (event.type == pygame.MOUSEBUTTONDOWN):
-                if (help_button.checkForInput(mouse_pos)):
-                    find_move = True
-                    print("needs help finding move!")
+                running = False       
         for i in range(len(new_grid)):
             for j in range(len(new_grid[i])):
                 new_grid[i][j].update(screen)
                 new_grid[i][j].move(events_list)
-                if (find_move and new_grid[i][j].validateGrid()):
-                    print("There is more at {}".format((i, j)))
-                    help_text = font.render("There is a move at: {}".format(i+1, j+1), True, green)
-                    screen.blit(help_text, help_rect)
-
-                    find_move = False
-
-
-                
-        
-        pygame.display.flip()
         pygame.display.update()
 
