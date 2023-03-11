68a801598eb5fda79e5a2be731c2adbf94e45aac
frankvp <90110502+frankvp11@users.noreply.github.com>
2023-03-11 18:24:01 -0500

    Add files via upload

--- website/game.js
+++ website/game.js
@@ -14,7 +14,7 @@ const candy_codes = {
 };
 
 const numOfRows = 9;
-const numOfCols = 12;
+const numOfCols = 9;
 
 let grid;
 let gridContainerElem;
@@ -38,7 +38,12 @@ function init() {
     createGridArray();
     createImageGrid();
     printGridArray();
-    eraseValidMoves();
+    var keepgoing = true;
+    while (keepgoing){
+        keepgoing = eraseValidMoves();
+    }
+    console.log("Fully good array: ")
+    printGridArray();
     //  initEvents();
 }
 
@@ -76,6 +81,15 @@ function createImageGrid() {
         }
     }
 }
+function updateImages(){
+    for (let i = 0; i < grid.length; i++){
+        for (let j = 0; j < grid[i].length; j++){
+            var img = document.getElementById((i*12)+j);
+            img.src=  candy_codes[grid[i][j]];
+        }
+    }
+}
+
 
 function addImage(src, id) {
     // Create a container for the image element
@@ -94,14 +108,14 @@ function addImage(src, id) {
 
     // Set the 'alt' attribute using the 'src' file name
     // and the 'id' parameter.
-    let filename = src.replace(/^.*[\\\/]/, ''); // Remove path
-    filename = filename.split(".")[0]; // Remove extension
-    const splitWordsArr = filename.split(/(?=[A-Z])/);
-    const combinedWords = splitWordsArr.join(" ").toLowerCase();
-    img.setAttribute("alt", `${combinedWords} ${id}`);
+    //let filename = src.replace(/^.*[\\\/]/, ''); // Remove path
+    //filename = filename.split(".")[0]; // Remove extension
+    //const splitWordsArr = filename.split(/(?=[A-Z])/);
+    //const combinedWords = splitWordsArr.join(" ").toLowerCase();
+    //img.setAttribute("alt", `${combinedWords} ${id}`);
 
     // For debugging purposes
-    tdiv.setAttribute("data-color", splitWordsArr[0].toLowerCase());
+    //tdiv.setAttribute("data-color", splitWordsArr[0].toLowerCase());
 
     // Append image element to a div container
     tdiv.appendChild(img);
@@ -119,35 +133,55 @@ function printGridArray() {
         console.log(ranText);
     }
 }
-var dr = [1, -1, 0, 0];
-var dc = [0, 0, 1, -1];
+
 function dfs(i, j, grid, visited) {
 
     let original_Value = grid[i][j];
     let stack = [[i, j]];
     let size = 0;
     while (stack.length != 0) {
-        let [i, j] = stack.pop();
-        visited[i][j] = true;
+        let [x, y] = stack.pop();
+        visited[x][y] = true;
         size += 1;
-        if (i + 1 < grid.length && grid[i + 1][j] == original_Value && !visited[i + 1][j]) {
-            stack.push([i + 1, j]);
+        if (x + 1 < grid.length && grid[x + 1][y] == original_Value && !visited[x + 1][y]) {
+            stack.push([x + 1, y]);
         }
-        if (i - 1 > 0 && grid[i - 1][j] == original_Value && !visited[i - 1][j]) {
-            stack.push([i - 1, 0]);
+        if (x - 1 >= 0 && grid[x - 1][y] == original_Value && !visited[x - 1][y]) {
+            stack.push([x - 1, y]);
         }
-        if (j + 1 < grid[i].length && grid[i][j + 1] == original_Value && !visited[i][j + 1]) {
-            stack.push([i, j + 1]);
+        if (y + 1 < grid[x].length && grid[x][y + 1] == original_Value && !visited[x][y + 1]) {
+            stack.push([x, y + 1]);
         }
-        if (j - 1 > 0 && grid[i][j - 1] == original_Value && !visited[i][j - 1]) {
-            stack.push([i, j - 1]);
+        if (y - 1 >= 0 && grid[x][y - 1] == original_Value && !visited[x][y - 1]) {
+            stack.push([x, y - 1]);
         }
     }
     return size;
 
 }
+function changingDFS(i, j, grid){
+    let original_Value = grid[i][j];
+    let stack = [[i, j]];
 
+    while (stack.length != 0){
+        let [x,y] = stack.pop();
+        grid[x][y] = 0;
+        if (x + 1 < grid.length && grid[x + 1][y] == original_Value && !visited[x + 1][y]) {
+            stack.push([x + 1, y]);
+        }
+        if (x - 1 >= 0 && grid[x - 1][y] == original_Value && !visited[x - 1][y]) {
+            stack.push([x - 1, y]);
+        }
+        if (y + 1 < grid[x].length && grid[x][y + 1] == original_Value && !visited[x][y + 1]) {
+            stack.push([x, y + 1]);
+        }
+        if (y - 1 >= 0 && grid[x][y - 1] == original_Value && !visited[x][y - 1]) {
+            stack.push([x, y - 1]);
+        }
+    }
+}
 function eraseValidMoves() {
+    var changesDone = false;
     visited = new Array(numOfRows);
     for (let i = 0; i < numOfRows; i++) {
         visited[i] = new Array(numOfCols);
@@ -161,19 +195,41 @@ function eraseValidMoves() {
             if (!visited[i][j]) {
                 var size = dfs(i, j, grid, visited);
                 if (size >= 3) {
-                    console.log("There is a component bigger than 3 at: " + i + " " + j);
-                    grid[i][j] = 9;
-                    var img = document.getElementById(i*12+j);
-                    img.src = candy_codes[grid[i][j]];
-                    console.log(img.src);
+                    changingDFS(i,j , grid);
+                    gravity();
+                    changesDone = true;
                 }
             }
         }
     }
-
-
+    return changesDone;
 }
-
+function gravity(){
+    const maxNumOfCandyCodes = Object.keys(candy_codes).length - 6;
+    for (let j =0 ; j < grid[0].length; j++){
+        var temp = [];
+        for (let i = 0; i < grid.length; i++){
+            if (grid[i][j] == 0){
+                temp.unshift((Math.floor(Math.random() * maxNumOfCandyCodes) + 1));
+            }
+            else {
+                temp.push(grid[i][j]);
+            }
+        }    
+        for (let i = 0; i < temp.length; i++){
+            grid[i][j] = temp.shift();
+            var img = document.getElementById((i*12)+j);
+            img.src = candy_codes[grid[i][j]];
+        }    
+    }
+    //eraseValidMoves();
+}
+function tempSwap(i1, j1, i2, j2){
+    var temp_grid_val1 = grid[i1][j1];
+    var temp_grid_val2 = grid[i2][j2];
+    grid[i1][j1] = temp_grid_val2;
+    grid[i2][j2] = temp_grid_val1;
+}
 // ------------------------------------------------------
 //  Drag and drop
 
@@ -207,9 +263,48 @@ function dropSwapOrMove(ev) {
     // since 'sourceImageElem.closest(".image-container")'
     // returns the image container of the source image
     // BEFORE it's moved to the target image container.
-    if ((Math.abs(sourceId - targetId) == 1) || (Math.abs(sourceId - targetId) == 12)) {
+    if (sourceId - targetId == 1) {
         console.log("Good swap!");
 
+
+        sourceContainerElem.appendChild(targetImageElem);
+        sourceContainerElem.setAttribute("data-color", targetColor);
+
+        // 'ev.currentTarget' represents the image container
+        // (i.e. div.image-container)
+        targetContainerElem.appendChild(sourceImageElem);
+        targetContainerElem.setAttribute("data-color", sourceColor);
+        console.log(`SWAP ${sourceId} (source) with ${targetId} (target)`);
+    }
+    else if (sourceId - targetId == -1) {
+        console.log("Good swap!");
+ 
+        sourceContainerElem.appendChild(targetImageElem);
+        sourceContainerElem.setAttribute("data-color", targetColor);
+
+        // 'ev.currentTarget' represents the image container
+        // (i.e. div.image-container)
+        targetContainerElem.appendChild(sourceImageElem);
+        targetContainerElem.setAttribute("data-color", sourceColor);
+
+        console.log(`SWAP ${sourceId} (source) with ${targetId} (target)`);
+    }
+    else if (sourceId- targetId == 12){
+        console.log("Good swap!");
+
+        sourceContainerElem.appendChild(targetImageElem);
+        sourceContainerElem.setAttribute("data-color", targetColor);
+
+        // 'ev.currentTarget' represents the image container
+        // (i.e. div.image-container)
+        targetContainerElem.appendChild(sourceImageElem);
+        targetContainerElem.setAttribute("data-color", sourceColor);
+        console.log(tempi1 + " " + tempj1 + "Value: " + grid[tempi1][tempj1]);
+        console.log(`SWAP ${sourceId} (source) with ${targetId} (target)`);
+    }
+    else if (sourceId- targetId == -12){
+        console.log("Good swap!");
+        
         sourceContainerElem.appendChild(targetImageElem);
         sourceContainerElem.setAttribute("data-color", targetColor);
 
